143def96d8e4f692b273aa536ef16d95129a7482
Étienne Beaulac <etienne.beaulac@outlook.com>
2024-02-11 16:09:50 -0500

    Cleaned

--- robot.py
+++ robot.py
@@ -5,18 +5,15 @@ import wpilib.drive
 
 class Robot(wpilib.TimedRobot):
     def robotInit(self):
-        self._motor_left = wpilib.VictorSP(0)
-
-        self._motor_right = wpilib.VictorSP(1)
-
-        self._drive = wpilib.drive.DifferentialDrive(self._motor_left, self._motor_right)
-        wpilib.SmartDashboard.putData("Drive", self._drive)
-
         self.joystick = wpilib.Joystick(0)
+
+        self._motor_left = wpilib.VictorSP(0)
+        self._motor_right = wpilib.VictorSP(1)
+        self._drive = wpilib.drive.DifferentialDrive(
+            self._motor_left, self._motor_right
+        )
+
+        wpilib.SmartDashboard.putData("Drive", self._drive)
 
     def teleopPeriodic(self) -> None:
         self._drive.arcadeDrive(-1 * self.joystick.getY(), -1 * self.joystick.getX())
-
-
-if __name__ == "__main__":
-    wpilib.run(Robot)
